cfa2468623ec1ee1fd6d37c7dce614c6a8a39bf1
meg44k <yugomegane@gmail.com>
2025-06-23 15:27:09 +0900

    Fix: RegisterのつづりがRegistorになっていたので修正しました

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\RegistorController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LoginController::class, 'view']);
 
 
-Route::get('/registor', [RegistorController::class,'view']);
+Route::get('/register', [RegisterController::class,'view']);
 
 Route::get('/login', [LoginController::class,'view']);
 
