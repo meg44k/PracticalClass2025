b074fe9b01ceae7f9d4623012d4f6a49d66be56d
O_tea130 <fgg555890@outlook.com>
2025-06-24 09:38:42 +0900

    Add: メインページのcssファイルをpublicに追加しました

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,75 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>楽市楽打メイン画面</title>
-    <style>
-        body {
-            background-color:rgb(251, 170, 213);
-            font-family: sans-serif;
-            color: #333;
-
-            margin: 0;
-
-            display: flex;
-            justify-content:center;
-            align-items: center;
-
-            height: 100vh;
-        }
-        .content-wrapper {
-            position: absolute;
-            top:50;
-            right:50;
-
-            background-color: white;
-            padding: 50px 60px;
-            border-radius: 10px;
-        }
-        .site-title {
-            margin: 0; /* タイトルの下に余白 */
-            margin-bottom: 30px;
-            font-size: 40px;
-        } 
-        .auth-links {
-            display: flex;
-            flex-direction: column;
-            gap: 50px;
-        }
-        .auth-links a {
-        
-            display: block;
-            text-decoration: none;
-            font-weight: bold;
-            color:rgb(73, 65, 65);
-            /*background-color: #ffffff*/
-            padding: 3px 3px;
-            border-radius: 50px;
-        }
-        .mypage-icon {
-            position: absolute;
-            top: 2px;
-            right: 2px;
-            font-size: 25px;
-        }
-        .setting-icon {
-            position: absolute;
-            top: 2px;
-            right:60px;
-            font-size: 25px;
-        }
-        .battle-menu {
-            font-size: 80px;
-        }
-        .back-menu {
-            position: absolute;
-            top: 5px;
-            left:5px;
-            font-size:25px;
-        }
-        .auth-links a:hover {
-            background-color:rgb(231, 136, 214);
-        }
-        
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 </head>
 <body>
 
