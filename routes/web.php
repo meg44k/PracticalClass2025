3b6707dc5b1fa5d57e64159a42de879477811a76
meg44k <yugomegane@gmail.com>
2025-06-30 15:20:31 +0900

    Add: ルーティング設定が消えていたのを修正

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -18,3 +14,26 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('/', function () {
+    return view('title');
+});
+Route::get('/result', function () {
+    return view('result');
+});
+
+Route::get('/mypage', function () {
+    return view('mypage');
+});
+
+Route::get('/settings', function () {
+    return view('settings');
+});
+
+Route::get('/battle', function () {
+    return view('battle');
+});
+
+Route::get('/main', function () {
+    return view('main');
+});
