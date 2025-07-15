614de71cdf615dc99d868047f8329f79f765c0af
meg44k <yugomegane@gmail.com>
2025-07-15 11:06:12 +0900

    Add: 世界ランキングを追加し、スコアボードをスクロールできるようにしました

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\GameScore;
 
+use App\Models\User;
+
 class GameController extends Controller
 {
     public function store(Request $request)
@@ -31,6 +33,12 @@ class GameController extends Controller
                                ->orderByDesc('score')
                                ->get();
 
-        return view('mypage', compact('gameScores'));
+        $worldRanking = GameScore::with('user')
+                                 ->selectRaw('id, MAX(score) as high_score')
+                                 ->groupBy('id')
+                                 ->orderByDesc('high_score')
+                                 ->get();
+
+        return view('mypage', compact('gameScores', 'worldRanking'));
     }
 }
--- app/Models/GameScore.php
+++ app/Models/GameScore.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class GameScore extends Model
 {
     protected $fillable = ['id','score','type_count','missed_type_count'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'id', 'id');
+    }
 }
