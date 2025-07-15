b91863b9d0c09e295c5e405f5c8e9e4188d3b112
ytadjjfaks <k21030au@apps.kct.ac.jp>
2025-07-15 10:26:50 +0900

    リザルト画面を作った

---
+++ resources/views/result.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>結果</title>
+    <link rel="stylesheet" href="{{ asset('css/result.css') }}">
+</head>
+<body>
+
+    <header class="header">
+        <h1 class="result_finish">最終結果</h1>
+        <h1 class="result_ranking">ランキング</h1>
+        <div class="auth-links">
+            <a href="main" class="custom-link back-menu">
+                <span class="icon">🔙</span>
+            </a>
+        </div>
+    </header>
+
+    <div class="content-squares-container"> <div class="white-square result-content">
+            <p>スコア</p>
+            <p>総タイピング数</p>
+            <p>ミスタイプ数</p>
+            <p>ミスタイプ率</p>
+            <p>予備</p>
+        </div>
+        <div class="white-square ranking-content">
+            <p>ランキング1位</p>
+            <p>ランキング2位</p>
+            <p>ランキング3位</p>
+            <p>ランキング4位</p>
+            <p>ランキング5位</p>
+        </div>
+    </div>
+
+</body>
+</html>
+
+<!-- <!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>結果</title>
+    <link rel="stylesheet" href="{{ asset('css/result.css') }}">
+</head>
+<body>
+
+    <header class="header">
+        <h1 class="result_finish">最終結果</h1>
+        <h1 class="result_ranking">ランキング</h1>
+        <h2 class="table_finish">最終結果</h2>
+        <h2 class="table_ranking">ランキング</h2> -->
+        <!-- <div class="auth-links">
+                <a href="main" class="custom-link back-menu">
+                    <span class="icon">🔙</span>
+                </a>
+        </div>
+    </header>
+
+    </body>
+</html> --> 
